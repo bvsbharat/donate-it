260d0450e3a417264afa1b6629599608073c966f
Bhavnasi Venkat Satya <bvsbharat@hotmail.com>
2018-06-30 21:37:04 +0530

    profile icon change

--- src/app/shared/profile-widget-container/profile-widget-container.component.ts
+++ src/app/shared/profile-widget-container/profile-widget-container.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+import {CommonService} from '../../services/common.service'
 @Component({
   selector: 'app-profile-widget-container',
   templateUrl: './profile-widget-container.component.html',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileWidgetContainerComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private commonService: CommonService) { }
+  userName: string = "James";
+  userType: string = "Donor";
   ngOnInit() {
+    let userdetails=this.commonService.getUserInformation();
+        if(userdetails) {
+            var userName = userdetails.name;
+            this.userName = userName ? userName : this.userName;
+            this.userType = userdetails.type;
+        }
   }
 
 }
